7698fbc29f7323e3bc58e78095c6c47117a45a99
kobakhit <kobakhit@gmail.com>
2016-11-05 16:03:12 -0400

    added lib

---
+++ lib-BG.R
@@ -0,0 +1,114 @@
+# Author: Koba Khitalishvili
+#
+# I use Google's R style guide
+# https://google.github.io/styleguide/Rguide.xml#functiondefinition
+#
+# A set of functions based on Peter Fader's and Bruce Hardie's research in using shifted beta 
+# geometric distribution (sBG) to project customer retention
+
+# Paper 1. "How to project customer retention" Fader and Hardie (2007)
+# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.558.935&rep=rep1&type=pdf
+
+churnBG <- Vectorize(function(alpha,beta,period){
+  # Computes churn probabilities based on sBG distribution
+  # Equation (7) in Paper 1
+  #
+  # Args:
+  #  alpha: numeric
+  #  beta: numeric
+  #  period: integer or vector of integers
+  #
+  # Returns:
+  #  Vector of churn probabilities for period(s) 
+  t1 = alpha/(alpha+beta)
+  result = t1
+  if (period > 1) {
+    result = churnBG(alpha,beta,period-1)*(beta+period-2)/(alpha+beta+period-1)
+  }
+  
+  return(result)
+}, vectorize.args = c("period"))
+
+
+survivalBG = Vectorize(function(alpha, beta, period){
+  # Computes survival probabilites based on a sBG distribution
+  #
+  # Args:
+  #  alpha: numeric
+  #  beta: numeric
+  #  period: integer or vector of integers
+  #
+  # Returns:
+  #  Vector of survival probabilities for period(s) 
+  t1 = 1-churnBG(alpha,beta,1)
+  result = t1
+  if(period>1){
+    result = survivalBG(alpha,beta,period-1)-churnBG(alpha,beta,period)
+  }
+  return(result)
+}, vectorize.args = c("period"))
+
+
+estimateMLL <- function(alphabeta, active.cust, lost.cust) {
+  # Estimates MLL to fit the sBG distribution curve
+  #
+  # Args:
+  # alphabeta: vector with alpha being the first and beta being the second elements, c(a,b)
+  #  
+  
+  MLL = function(alphabeta){
+    # Computesl likelihood. Equation (B3) in Paper 1
+    #
+    # Args:
+    #  alphabeta: vector with alpha being the first and beta being the second elements, c(a,b)
+    #
+    # Returns:
+    #  Vector of churn probabilities for period(s) 
+    #
+    # Error handling
+    if(length(activeCust) != length(lostCust)){
+      stop("Variables activeCust and lostCust have different lengths: ",
+           length(activeCust), " and ", length(lostCust), ".")
+    }
+    # Example data for seven periods
+    # activeCust = c(869,743,653,593,551,517,491)
+    # lostCust = c(131,126,90,60,42,34,26)
+    t=length(activeCust) # number of periods
+    alpha = alphabeta[1]
+    beta = alphabeta[2]
+    return(-as.numeric(
+      sum(lostCust*log(churnBG(alpha,beta,1:t))) +
+        activeCust[t]*log(survivalBG(alpha,beta,t))
+    ))
+  }
+  
+  activeCust = active.cust
+  lostCust = lost.cust
+  
+  return(optim(alphabeta, MLL))
+}
+
+
+estimateMLL2 <- function(active.cust, lost.cust) {
+  # Taken from 
+  # http://stats.stackexchange.com/questions/76678/how-to-write-log-likelihood-for-beta-geometric-with-optim-in-r
+  
+  active = active.cust
+  lost = lost.cust
+  
+  loop.lik<-function(params) {
+    a<-params[1]
+    b<-params[2]
+    ll<-0
+    for (i in 1:length(lost)) {
+      ll<-ll+lost[i]*log(beta(a+1,b+i-1)/beta(a,b))
+    }
+    ll<-ll+active[i]*log(beta(a,b+i)/beta(a,b))
+    return(-ll)    #return the negative of the function to maximize likelihood
+  } 
+  
+  return(optim(c(1,1), loop.lik))
+}
+
+
+
